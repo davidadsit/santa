9149731c32c2bdbbf51449ebb471d70b8f5f047f
davidadsit <david.adsit@gmail.com>
2020-12-01 18:52:27 -0700

    basic CLI scaffold

--- src/Program.cs
+++ src/Program.cs
@@ -2,11 +2,64 @@
 
 namespace Santa
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Add()
         {
-            Console.WriteLine("Hello World!");
+            throw new NotImplementedException();
+        }
+
+        private static void Help()
+        {
+            Console.Out.WriteLine("Commands:");
+            Console.Out.WriteLine("  add");
+            Console.Out.WriteLine("  remove");
+            Console.Out.WriteLine("  update");
+            Console.Out.WriteLine("  help");
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Santa, Inc.");
+            Console.Out.WriteLine("Welcome to The Digital Naughty & Nice List");
+            Console.Out.WriteLine("");
+
+            var command = "help";
+            do
+            {
+                switch (command)
+                {
+                    case "add":
+                        Add();
+                        break;
+                    case "update":
+                        Update();
+                        break;
+                    case "remove":
+                        Remove();
+                        break;
+                    default:
+                        Help();
+                        break;
+                }
+
+                Console.Out.WriteLine("");
+                Console.Out.Write("Enter a command: ");
+                command = Console.In.ReadLine()?.Split(" ")[0].ToLower() ?? "help";
+            } while (command != "exit");
+
+            Console.WriteLine("Thank you for using The Digital Naughty & Nice List");
+            Console.WriteLine("");
+        }
+
+        private static void Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Update()
+        {
+            throw new NotImplementedException();
         }
     }
 }
